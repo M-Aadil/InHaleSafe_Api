d1d4d4e443e12bed6a4b56b2f52b1b0d678f4ee4
Md Aadil Ansari <aadilansari2052@gmail.com>
2024-04-04 02:52:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const app = express(); // Creating an express object
 const port = 8000; // Setting a port for this application
 
 let sensorData = []; // Array to store sensor data
-const MAX_SENSOR_DATA = 30;
 
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
@@ -39,10 +38,6 @@ app.post('/sensor-data', function (req, res) {
 
     // Add received data to sensorData array
     sensorData.push(jsonData);
-
-    if (sensorData.length > MAX_SENSOR_DATA) {
-        sensorData = [];
-    }
 
     // Send the updated sensor data back to the client as JSON
     res.json(sensorData);
